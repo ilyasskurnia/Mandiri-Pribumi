6399da6dc81665c58f5f3525d51972823dc78498
ilyasskurnia <ilyaskurnia07@gmail.com>
2023-12-04 16:27:40 +0700

    backend portal

--- resources/views/Portal/pages/detailartikel.blade.php
+++ resources/views/Portal/pages/detailartikel.blade.php
@@ -66,7 +66,7 @@
             </div>
             <div class="col-md-3 animate-box" data-animate-effect="fadeInRight">
                 <div>
-                    <div class="fh5co_heading fh5co_heading_border_bottom py-2 mb-4">Tags</div>
+                    <div class="fh5co_heading fh5co_heading_border_bottom py-2 mb-4">Kategori</div>
                 </div>
                 <div class="clearfix"></div>
                 <div class="fh5co_tags_all">
